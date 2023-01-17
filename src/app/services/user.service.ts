6af51cf494e1bf4517dc80c0a619bf6cc15d9a67
BrandonMcKay <brandonmckay07@gmail.com>
2023-01-16 23:22:39 -0800

    sign in functionality

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -12,6 +12,11 @@ export class UserService {
   signUp(user:any):Observable<string>{
     return this.http.post("http://localhost:9090/user/signUp",user,{responseType:'text'})
   }
+
+  signIn(user:any):Observable<string> {
+    return this.http.post("http://localhost:9090/user/signIn",user,{responseType:'text'});
+  }
+
 }
 
 //keep going Brandon
